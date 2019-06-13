7d697e50487adec68de5f84416b39529340dd20b
Haley DeKeyser <hcd09@live.com>
2019-06-13 00:19:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM series
   INNER JOIN authors, subgenres
-  ON authors.id = series.author_id, subgenres.id = series.subgenre_id;"
+  ON (authors.id = series.author_id) (subgenres.id = series.subgenre_id);"
 end
 
 def select_series_title_with_most_human_characters
