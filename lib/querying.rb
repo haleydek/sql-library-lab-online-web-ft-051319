4d27dcae8714d38cea3af006bcc56d3f3ed85d90
Haley DeKeyser <hcd09@live.com>
2019-06-13 00:06:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,6 +22,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
+  #series.author_id = authors.id
+  #authors.name
+  #series.subgenre_id = subgenres.id
+  #subgenres.name
   ""
 end
 
